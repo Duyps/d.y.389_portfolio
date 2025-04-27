875a9f6fc8e1de12370b05fb2f31bc3e3b3140ef
Duy <quocduyps1@gmail.com>
2025-04-27 22:31:14 +0700

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Duy-portfolio",
+  base: "/d.y.389_portfolio",
   plugins: [react()],
 })
